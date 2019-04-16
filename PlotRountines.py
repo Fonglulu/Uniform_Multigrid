2b41746de3c5f1c675de8a0027870061e5b24c95
Fonglulu <44454829+Fonglulu@users.noreply.github.com>
2019-04-16 19:01:46 +1000

    Updated on 16/4/2019

--- PlotRountines.py
+++ PlotRountines.py
@@ -12,7 +12,6 @@ def smoother_plot(u):
     
     from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot as plt
-    #from matplotlib import cm
     from matplotlib.ticker import LinearLocator, FormatStrFormatter
     import numpy as np
     
